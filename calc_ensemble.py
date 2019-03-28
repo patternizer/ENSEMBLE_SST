Add barplot for parameters + fix scaling in covariance matrix heatmap

calc_ensemble.py
@@ -5,8 +5,8 @@
 # call as: python calc_ensemble.py
 
 # =======================================
-# Version 0.3
-# 26 March, 2019
+# Version 0.4
+# 28 March, 2019
 # michael.taylor AT reading DOT ac DOT uk
 # =======================================
 
@@ -16,6 +16,7 @@ import glob
 import optparse 
 from  optparse import OptionParser  
 import sys   
+import math
 import numpy as np
 import numpy.ma as ma  
 import xarray
@@ -29,7 +30,17 @@ from scipy.sparse.linalg import eigsh
 import seaborn as sns; sns.set(style="darkgrid")
 import matplotlib.pyplot as plt; plt.close("all")
 import matplotlib.dates as mdates
+import matplotlib.colors as colors
+import matplotlib.ticker as ticker
 
+# =======================================    
+# AUXILIARY METHODS
+# =======================================    
+def fmt(x, pos):
+    a, b = '{0:.3e}'.format(x).split('e')
+    b = int(b)
+
+    return r'${} \times 10^{{{}}}$'.format(a, b)
 # =======================================    
 
 def load_data(file_in):
@@ -45,50 +56,46 @@ def load_data(file_in):
 
     return ds
 
-def calc_eigen(ds):
+def plot_parameters(ds):
     '''
-    Calculate eigenvalues and eigenvectors from the harmonisation parameter covariance matrix
+    Plot harmonisation parameters of best-case
     '''
-    # Harmonisation parameter covariance matrix: (27, 27)
-    parameter_covariance_matrix = ds['parameter_covariance_matrix'] 
-    X = parameter_covariance_matrix
+    # Harmonisation parameters: (27,)
+    parameter = ds['parameter'] 
 
-    eigenval, eigenvec = np.linalg.eig(X)
-#    print('Eigenvalues \n%s' %eigenval)   
-#    print('Eigenvectors \n%s' %eigenvec)
+    sensor = ['METOPA','NOAA19','NOAA18','NOAA17','NOAA16','NOAA15','NOAA14','NOAA12','NOAA11']
 
-    return eigenval, eigenvec
+    X = np.arange(0,9)
+    Y = np.array(parameter)
+    
+    idx0 = np.arange(0, len(Y), 3)        
+    idx1 = np.arange(1, len(Y), 3)        
+    idx2 = np.arange(2, len(Y), 3)        
+    df = pd.DataFrame()
+    Y0 = []
+    Y1 = []
+    Y2 = []
+    for i in range(0,len(X)): 
+        k0 = idx0[i]
+        k1 = idx1[i]
+        k2 = idx2[i]
+        Y0.append(Y[k0])
+        Y1.append(Y[k1])
+        Y2.append(Y[k2])
+    Y = np.array([Y0,Y1,Y2])    
 
-def plot_eigenval(eigenval):
-    '''
-    Plot eigenvalues as a scree plot
-    '''
-    Y = eigenval / max(eigenval)
-    N = len(Y)    
-    X = np.arange(0,N)
+    df = pd.DataFrame({'a(0)': Y0, 'a(1)': Y1, 'a(2)': Y2}, index=list(sensor))                  
+    ax = df.plot(kind="bar", subplots=True, layout=(3, 1), sharey=False, sharex=True, rot=90, fontsize=12, legend=False)
+    fig = ax[0][0].get_figure()
+    ax0 = fig.add_subplot(111, frame_on=False)   # creating a single axes
+    ax0.set_xticks([])
+    ax0.set_yticks([])
+    for i,axi in np.ndenumerate(ax):
+        axi.set_title(axi.get_title(),{'size' : 12}) 
 
-    fig = plt.figure()
-    plt.fill_between(X, Y, step="post", alpha=0.4)
-    plt.plot(X, Y, drawstyle='steps-post')
-    plt.tick_params(labelsize=12)
-    plt.ylabel("Relative value", fontsize=12)
-    title_str = 'Scree plot: eigenvalue max=' + "{0:.5f}".format(eigenval.max())
-    plt.title(title_str)
-    plt.savefig('eigenvalues.png')    
-
-def plot_eigenvec(eigenvec):
-    '''
-    Plot eigenvector matrix as a heatmap
-    '''
-    X = eigenvec
-
-    fig = plt.figure()
-    sns.heatmap(X, center=0, linewidths=.5, cmap="viridis", cbar=True)
-#    sns.heatmap(X, vmin=0, vmax=1, linewidths=.5, cmap="viridis", cbar=True)
-#    sns.heatmap(X, center=0, linewidths=.5, annot=True, fmt="f", cmap="viridis", cbar=True)
-
-    plt.title('Eigenvector matrix')
-    plt.savefig('eigenvectors.png')    
+    plt.tight_layout()
+    file_str = "bestcase_parameters.png"
+    plt.savefig(file_str)    
 
 def plot_covariance(ds):
     '''
@@ -97,77 +104,33 @@ def plot_covariance(ds):
     # Harmonisation parameter covariance matrix: (27, 27)
     parameter_covariance_matrix = ds['parameter_covariance_matrix'] 
 
-    X = parameter_covariance_matrix
+    X = np.array(parameter_covariance_matrix)
+    Xmin = X.min()    
+    Xmax = X.max()    
+#    Y = X / Xmax
+    Y = X
 
     fig = plt.figure()
-#    sns.heatmap(X, center=0, linewidths=.5, cmap="viridis", cbar=True)
+#    log_norm = colors.LogNorm(vmin=Xmax, vmax=Xmax)
+#    cbar_ticks = [(10 ** i) for i in range(-6,1)]  
+#    sns.heatmap(Y, center=0.5, linewidths=.5, cmap="viridis", cbar=True, norm=log_norm, cbar_kws={"ticks":cbar_ticks})
+#    sns.heatmap(Y, center=0, linewidths=.5, cmap="viridis", cbar=True, vmin=-1.0e-9, vmax=1.0e-6, cbar_kws={"extend":'both', "format":'%.3e'})
+
+    sns.heatmap(Y, center=0, linewidths=.5, cmap="viridis", cbar=True, vmin=-1.0e-9, vmax=1.0e-6, cbar_kws={"extend":'both', "format":ticker.FuncFormatter(fmt)})
+
 
     #
     # Mask out upper triangle
     #
 
-    mask = np.zeros_like(X)
-    mask[np.triu_indices_from(mask)] = True
-    with sns.axes_style("white"):
-        sns.heatmap(X, mask=mask, square=True, center=0, linewidths=.5, cmap="viridis", cbar=True)
+    # mask = np.zeros_like(X)
+    # mask[np.triu_indices_from(mask)] = True
+    # with sns.axes_style("white"):
+    #     sns.heatmap(Y, mask=mask, square=True, center=0, linewidths=.5, cmap="viridis", cbar=True)
 
-    plt.title('Covariance matrix')
-    plt.savefig('covariance_matrix.png')    
-
-def plot_sample_binormal():
-    '''
-    # -------------------------------
-    # TEST CASE: SAMPLE FROM BINORMAL
-    # -------------------------------
-    '''
-
-    #
-    # Generate random binormal data
-    #
-    Xmean = np.zeros(2) # [0,0]
-    Xcov = np.eye(2) # [[1,0],[0,1]]
-    size = 10000
-    data1 = np.random.multivariate_normal(Xmean, Xcov, size)
-
-    #
-    # Make 100 draws 
-    #
-
-#    X1 = np.random.rand(100)
-#    X2 = np.random.rand(100)
-    X1 = data1[:,0]
-    X2 = data1[:,1]
-    Xmean = [X1.mean(), X2.mean()]
-    X = np.stack((X1, X2), axis=0)
-    Xcov = np.cov(X)
-#    Xcov = [[0.9,0.1],[0.1,0.9]]
-    print(Xmean)
-    print(Xcov)
-    size = 100
-    data2 = np.random.multivariate_normal(Xmean, Xcov, size)        
-    df1 = pd.DataFrame(data1, columns=['x1', 'x2'])
-    df2 = pd.DataFrame(data2, columns=['y1', 'y2'])
-
-    #
-    # Plot joint distribution
-    #
-
-    # colours = ["#9b59b6", "#3498db", "#95a5a6", "#e74c3c", "#34495e", "#2ecc71"]
-    #             purple,      blue,      grey,    orange,      navy,     green
-    fig, ax = plt.subplots()
-    graph = sns.jointplot(x=df1.x1, y=df1.x2, kind="hex", space=0, color="#3498db")
-    plt.subplots_adjust(left=0.1, right=0.8, top=0.9, bottom=0.1)
-    cax = graph.fig.add_axes([.81, .15, .02, .5])  # x, y, width, height
-    cbar = plt.colorbar(cax=cax)
-    cbar.set_label('count')
-    graph.x = df2.y1
-    graph.y = df2.y2
-    graph.plot_joint(plt.scatter, marker="x", color="#e74c3c", s=2)
-    graph.x = X1.mean()
-    graph.y = X2.mean()
-    graph.plot_joint(plt.scatter, marker="x", color="r", s=50)    
-    fig.suptitle('2D-sampling from binormal distribution')
-    plt.savefig('sampled_binormal.png')    
+    title_str = "Covariance matrix: max=" + "{0:.3e}".format(Xmax)
+    plt.title(title_str)
+    plt.savefig('bestcase_covariance_matrix.png')    
 
 def calc_ensemble(ds):
     '''
@@ -227,9 +190,51 @@ def calc_ensemble(ds):
 
     return draws
 
-def plot_ensemble(ds, draws):
+def plot_ensemble_histograms(ds, draws):
     '''
-    Plot ensemble
+    Plot histograms of ensemble coefficient deltas
+    '''
+    # Harmonisation parameters: (27,)
+    parameter = ds['parameter'] 
+    # Harmonisation parameter covariance matrix: (27, 27)
+    parameter_covariance_matrix = ds['parameter_covariance_matrix'] 
+
+    X = parameter
+    Y = draws
+    N = len(draws)
+    M = len(X)
+
+    sensor = ['METOPA','NOAA19','NOAA18','NOAA17','NOAA16','NOAA15','NOAA14','NOAA12','NOAA11']
+
+    #
+    # Histograms of ensemble variability
+    #
+
+    for i in range(0,9):
+
+        fig, ax = plt.subplots(3,1,sharex=True)
+        for j in range(0,3):
+            k = (3*i)+j
+            a_mean = Y[:,k].mean()
+            Z = 100.0 * (Y[:,k] - a_mean) / a_mean
+            hist, bins = np.histogram(Z, bins=100, density=False) 
+#            hist = hist/hist.sum()
+            ax[j].fill_between(bins[0:-1], hist, step="post", alpha=0.4)
+            ax[j].plot(bins[0:-1], hist, drawstyle='steps-post')
+#            ax[j].plot((a_mean,a_mean), (0,hist.max()), 'r-')   
+            ax[j].plot((0,0), (0,hist.max()), 'r-')   
+            ax[j].tick_params(labelsize=10)
+            ax[j].set_ylabel('Count', fontsize=10)
+            title_str = sensor[i] + ": a(" + str(j+1) + ")=" + "{0:.3e}".format(a_mean)
+            ax[j].set_title(title_str, fontsize=10)
+            
+        plt.xlabel(r'$\Delta$a / %', fontsize=10)
+        file_str = "ensemble_histograms_" + sensor[i] + ".png"
+        plt.savefig(file_str)    
+
+def plot_ensemble_coefficients(ds, draws):
+    '''
+    Plot ensemble coefficient deltas
     '''
     # Harmonisation parameters: (27,)
     parameter = ds['parameter'] 
@@ -253,43 +258,125 @@ def plot_ensemble(ds, draws):
         for j in range(0,3):
             k = (3*i)+j
             a_mean = Y[:,k].mean()
-            ax[j].plot(np.arange(0,N),(100.0 * (Y[:,k] - a_mean) / a_mean))            
+            Z = (100.0 * (Y[:,k] - a_mean) / a_mean)
+            ax[j].plot(np.arange(0,N),Z)            
             ax[j].plot((0,N), (0,0), 'r-')   
-#            ax[j].plot((0,N), (a_mean,a_mean), 'r-')   
             ax[j].tick_params(labelsize=10)
-            ax[j].set_ylabel('Anomaly / %', fontsize=10)
-            title_str = sensor[i] + ": coeff " + str(j+1) + "=" + "{0:.3e}".format(a_mean)
+            ax[j].set_ylabel(r'$\Delta$a / %', fontsize=10)
+            title_str = sensor[i] + ": a(" + str(j+1) + ")=" + "{0:.3e}".format(a_mean)
             ax[j].set_title(title_str, fontsize=10)
 
         plt.xlabel('Ensemble member', fontsize=10)
         file_str = "ensemble_coefficients_" + sensor[i] + ".png"
         plt.savefig(file_str)    
 
+# =======================================    
+# AUXILIARY STATISTICS
+# =======================================    
+
+def calc_eigen(ds):
+    '''
+    Calculate eigenvalues and eigenvectors from the harmonisation parameter covariance matrix
+    '''
+    # Harmonisation parameter covariance matrix: (27, 27)
+    parameter_covariance_matrix = ds['parameter_covariance_matrix'] 
+    X = parameter_covariance_matrix
+
+    eigenval, eigenvec = np.linalg.eig(X)
+#    print('Eigenvalues \n%s' %eigenval)   
+#    print('Eigenvectors \n%s' %eigenvec)
+
+    return eigenval, eigenvec
+
+def plot_eigenval(eigenval):
+    '''
+    Plot eigenvalues as a scree plot
+    '''
+    Y = eigenval / max(eigenval)
+    N = len(Y)    
+    X = np.arange(0,N)
+
+    fig = plt.figure()
+    plt.fill_between(X, Y, step="post", alpha=0.4)
+    plt.plot(X, Y, drawstyle='steps-post')
+    plt.tick_params(labelsize=12)
+    plt.ylabel("Relative value", fontsize=12)
+    title_str = 'Scree plot: eigenvalue max=' + "{0:.5f}".format(eigenval.max())
+    plt.title(title_str)
+    plt.savefig('bestcase_eigenvalues.png')    
+
+def plot_eigenvec(eigenvec):
+    '''
+    Plot eigenvector matrix as a heatmap
+    '''
+    X = eigenvec
+
+    fig = plt.figure()
+    sns.heatmap(X, center=0, linewidths=.5, cmap="viridis", cbar=True)
+#    sns.heatmap(X, vmin=0, vmax=1, linewidths=.5, cmap="viridis", cbar=True)
+#    sns.heatmap(X, center=0, linewidths=.5, annot=True, fmt="f", cmap="viridis", cbar=True)
+
+    plt.title('Eigenvector matrix')
+    plt.savefig('bestcase_eigenvectors.png')    
+
+def plot_sample_binormal():
+    '''
+    # -------------------------------
+    # TEST CASE: SAMPLE FROM BINORMAL
+    # -------------------------------
+    '''
+
     #
-    # Histograms of ensemble variability
+    # Generate random binormal data
+    #
+    Xmean = np.zeros(2) # [0,0]
+    Xcov = np.eye(2) # [[1,0],[0,1]]
+    size = 10000
+    data1 = np.random.multivariate_normal(Xmean, Xcov, size)
+
+    #
+    # Make 100 draws 
     #
 
-    for i in range(0,9):
+#    X1 = np.random.rand(100)
+#    X2 = np.random.rand(100)
+    X1 = data1[:,0]
+    X2 = data1[:,1]
+    Xmean = [X1.mean(), X2.mean()]
+    X = np.stack((X1, X2), axis=0)
+    Xcov = np.cov(X)
+#    Xcov = [[0.9,0.1],[0.1,0.9]]
+    print(Xmean)
+    print(Xcov)
+    size = 100
+    data2 = np.random.multivariate_normal(Xmean, Xcov, size)        
+    df1 = pd.DataFrame(data1, columns=['x1', 'x2'])
+    df2 = pd.DataFrame(data2, columns=['y1', 'y2'])
 
-        fig, ax = plt.subplots(3,1,sharex=False)
-        for j in range(0,3):
-            k = (3*i)+j
-            a_mean = Y[:,k].mean()
-            Z = 100.0 * (Y[:,k] - a_mean) / a_mean
-            hist, bins = np.histogram(Z, bins=100, density=False) 
-#            hist = hist/hist.sum()
-            ax[j].fill_between(bins[0:-1], hist, step="post", alpha=0.4)
-            ax[j].plot(bins[0:-1], hist, drawstyle='steps-post')
-#            ax[j].plot((a_mean,a_mean), (0,hist.max()), 'r-')   
-            ax[j].plot((0,0), (0,hist.max()), 'r-')   
-            ax[j].tick_params(labelsize=10)
-            ax[j].set_ylabel('Count', fontsize=10)
-            title_str = sensor[i] + ": coefficient=" + str(j+1)
-            ax[j].set_title(title_str, fontsize=10)
+    #
+    # Plot joint distribution
+    #
 
-        plt.xlabel('Coefficient value', fontsize=10)
-        file_str = "ensemble_histograms_" + sensor[i] + ".png"
-        plt.savefig(file_str)    
+    # colours = ["#9b59b6", "#3498db", "#95a5a6", "#e74c3c", "#34495e", "#2ecc71"]
+    #             purple,      blue,      grey,    orange,      navy,     green
+    fig, ax = plt.subplots()
+    graph = sns.jointplot(x=df1.x1, y=df1.x2, kind="hex", space=0, color="#3498db")
+    plt.subplots_adjust(left=0.1, right=0.8, top=0.9, bottom=0.1)
+    cax = graph.fig.add_axes([.81, .15, .02, .5])  # x, y, width, height
+    cbar = plt.colorbar(cax=cax)
+    cbar.set_label('count')
+    graph.x = df2.y1
+    graph.y = df2.y2
+    graph.plot_joint(plt.scatter, marker="x", color="#e74c3c", s=2)
+    graph.x = X1.mean()
+    graph.y = X2.mean()
+    graph.plot_joint(plt.scatter, marker="x", color="r", s=50)    
+    fig.suptitle('2D-sampling from binormal distribution')
+    plt.savefig('sampled_binormal.png')    
+
+# =======================================    
+# MAIN BLOCK
+# =======================================    
     
 if __name__ == "__main__":
 
@@ -300,13 +387,14 @@ if __name__ == "__main__":
     file_in = "harm_FO_3.0_27b18eb_ODR_101_11_R____AVHRR_REAL_4_RSA______19911004_20151231.nc"
     ds = load_data(file_in)
     draws = calc_ensemble(ds)
-    plot_ensemble(ds, draws)
-
     eigenval, eigenvec = calc_eigen(ds)
     plot_eigenval(eigenval)
     plot_eigenvec(eigenvec)
+    plot_parameters(ds)
     plot_covariance(ds)
     plot_sample_binormal()
+    plot_ensemble_histograms(ds, draws)
+    plot_ensemble_coefficients(ds, draws)
 
 
 