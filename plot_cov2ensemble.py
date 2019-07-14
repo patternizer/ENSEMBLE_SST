94d0d8396b6f1eea8f7660e7f6c290cfc37e9430
Michael Taylor <patternizer@gmail.com>
2019-07-15 00:52:22 +0100

    Plotting routines include code for cov2ensemble.py

---
+++ plot_cov2ensemble.py
@@ -0,0 +1,85 @@
+# !/usr/bin/env python
+
+# Code include segment in: cov2ensemble.py
+# ========================================
+# Version 0.1
+# 11 July, 2019
+# michael.taylor AT reading DOT ac DOT uk
+# ========================================
+
+def plot_eigenspectrum(ev):
+
+    nPC = ev['nPC']
+    fig,ax = plt.subplots()
+    plt.plot(ev['eigenvalues_rank'], ev['eigenvalues_norm'], linestyle='-', marker='.', color='b', label=r'$\lambda/\sum\lambda$')
+    plt.plot(ev['eigenvalues_rank'][nPC], ev['eigenvalues_norm'][nPC], marker='o', color='k', mfc='none',label=None)
+    plt.plot(ev['eigenvalues_rank'], ev['eigenvalues_cumsum'], linestyle='-', marker='.', color='r',label='cumulative')
+    plt.plot(ev['eigenvalues_rank'][nPC], ev['eigenvalues_cumsum'][nPC], marker='o', color='k', mfc='none',label='n(PC)='+str(nPC))
+    plt.legend(loc='right', fontsize=10)
+    plt.xlabel('rank')
+    plt.ylabel('relative variance', color='b')
+    plotstr = 'eigenspectrum'+plotstem
+    plt.savefig(plotstr)
+    plt.close('all')
+
+def plot_pc_deltas(dX,Xu):
+
+    fig,ax = plt.subplots()
+    for i in range(2*n):
+        labelstr_c = 'PC1 (constrained) ' + str(i+1)
+        plt.plot(dX['dX0_constrained'][i,:]/Xu, lw=2, label=labelstr_c)
+    for i in range(2*n):
+        labelstr_u = '(unconstrained) ' + str(i+1)
+        plt.plot(dX['dX0_unconstrained'][i,:]/Xu, '.', label=labelstr_u)
+#    plt.ylim(-2,2)
+    plt.legend(loc=2, fontsize=6, ncol=4)
+    plt.xlabel('parameter, a(n)')
+    plt.ylabel(r'$\delta a(n)/u(n)$')
+    plt.tight_layout()
+    plotstr = 'pc1_deltas_over_Xu'+plotstem
+    plt.savefig(plotstr)
+    plt.close('all')
+
+    fig,ax = plt.subplots()
+    for i in range(2*n):
+        labelstr_c = 'PC2 (constrained) ' + str(i+1)
+        plt.plot(dX['dX1_constrained'][i,:]/Xu, lw=2, label=labelstr_c)
+    for i in range(2*n):
+        labelstr_u = '(unconstrained) ' + str(i+1)
+        plt.plot(dX['dX1_unconstrained'][i,:]/Xu, '.', label=labelstr_u)
+#    plt.ylim(-2,2)
+    plt.legend(loc=2, fontsize=6, ncol=4)
+    plt.xlabel('parameter, a(n)')
+    plt.ylabel(r'$\delta a(n)/u(n)$')
+    plt.tight_layout()
+    plotstr = 'pc2_deltas_over_Xu'+plotstem
+    plt.savefig(plotstr)
+    plt.close('all')
+
+def plot_crs():
+
+    for n in np.array([10,50,100,500,1000,5000,10000,50000]):
+
+        random_numbers_unconstrained = crs.generate_10_single(n)
+        random_numbers_constrained = crs.generate_10(n)
+
+        fig,ax = plt.subplots(1,2)
+        labelstr_constrained = 'n=' + str(n) + ': constrained'
+        labelstr_unconstrained = 'n=' + str(n) + ': unconstrained'
+        ax[0].plot(np.sort(np.array(random_numbers_unconstrained)), label=labelstr_unconstrained)
+        ax[0].plot(np.sort(np.array(random_numbers_constrained)), label=labelstr_constrained)
+        ax[0].legend(loc=2, fontsize=8)
+        ax[0].set_ylim(-5,5)
+        ax[0].set_ylabel('z-score')
+        ax[0].set_xlabel('rank')
+        ax[0].set_title(r'Sorted random sample from $erf^{-1}(x)$')
+        ax[1].hist(random_numbers_unconstrained,bins=100,alpha=0.3,label='unconstrained')
+        ax[1].hist(random_numbers_constrained,bins=100,alpha=0.3,label='constrained')
+        ax[1].set_xlim(-5,5)
+        ax[1].set_xlabel('z-score')
+        ax[1].set_ylabel('count')
+        plotstr = 'random_numbers_n_' + str(n) + '.png'
+        plt.tight_layout()
+        plt.savefig(plotstr)
+        plt.close('all')
+
