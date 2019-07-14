ea9d7bf828e768078d162f7f72855aa19a1f9dd2
Michael Taylor <patternizer@gmail.com>
2019-07-15 00:51:58 +0100

    Moved plotting routines into plot_cov2ensemble.py

--- cov2ensemble.py
+++ cov2ensemble.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 # call as: python cov2ensemble.py 
+# include plot code: plot_cov2ensemble.py
   
 # =======================================
-# Version 0.8
-# 9 July, 2019
+# Version 0.10
+# 14 July, 2019
 # https://patternizer.github.io/
 # michael.taylor AT reading DOT ac DOT uk
 # =======================================
@@ -25,9 +26,11 @@ from mpl_toolkits import mplot3d
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d import proj3d
 
+#------------------------------------------------------------------------------
 import crs as crs            # constrained random sampling code: crs.py
 import cov2u as cov2u        # estimation of uncertainty from covariance matrix
 import convert_func          # measurement equations & L<-->BT conversion
+#------------------------------------------------------------------------------
 
 def FPE(x0,x1):
 
@@ -112,7 +115,7 @@ if __name__ == "__main__":
     #------------------------------------------------
     # OPTIONS
     #------------------------------------------------
-    ch = 37
+    ch = 12
     n = 5 # --> (2*n) = 10 = number of ensemble members
     c = 0.99 # variance_threshold
     N = 10000 # for draw matrix from Xcov
@@ -121,12 +124,10 @@ if __name__ == "__main__":
 
 #    software_tag = '3e8c463' # job dir=job_avhxx_v6_EIV_10x_11 (old runs)
 #    software_tag = '4d111a1' # job_dir=job_avhxx_v6_EIV_10x_11 (new runs)
-#    software_tag = 'v0.3Bet' # job_dir=job_avhxx_v6_EIV_10x_11 (new runs)
-    software_tag = 'v0.3Bet_all' # job_dir=job_avhxx_v6_EIV_10x_11 (new runs)
+    software_tag = 'v0.3Bet' # job_dir=job_avhxx_v6_EIV_10x_11 (new runs)
     plotstem = '_'+str(ch)+'_'+software_tag+'.png'
 
-#    harm_file = 'FIDUCEO_Harmonisation_Data_' + str(ch) + '.nc'    
-    harm_file = 'FIDUCEO_Harmonisation_Data_' + str(ch) + '_all' + '.nc'    
+    harm_file = 'FIDUCEO_Harmonisation_Data_' + str(ch) + '.nc'    
     ds = xarray.open_dataset(harm_file)
     Xave = np.array(ds.parameter)
     Xcov = np.array(ds.parameter_covariance_matrix)
@@ -135,84 +136,20 @@ if __name__ == "__main__":
 
     ev = cov2ev(Xcov,c)
     dX = calc_dX(n,ev['eigenvalues'],ev['eigenvectors'])
+    
+    # =======================================
+    # INCLUDE PLOT CODE:
+    exec(open('plot_cov2ensemble.py').read())
+    # =======================================
 
-    #------------------------------------------------
-    # PLOTS
-    #------------------------------------------------
+    plot_eigenspectrum(ev)
+    plot_pc_deltas(dX,Xu)
+    plot_crs()
 
-    nPC = ev['nPC']
-    fig,ax = plt.subplots()
-    plt.plot(ev['eigenvalues_rank'], ev['eigenvalues_norm'], linestyle='-', marker='.', color='b', label=r'$\lambda/\sum\lambda$')
-    plt.plot(ev['eigenvalues_rank'][nPC], ev['eigenvalues_norm'][nPC], marker='o', color='k', mfc='none',label=None)
-    plt.plot(ev['eigenvalues_rank'], ev['eigenvalues_cumsum'], linestyle='-', marker='.', color='r',label='cumulative')
-    plt.plot(ev['eigenvalues_rank'][nPC], ev['eigenvalues_cumsum'][nPC], marker='o', color='k', mfc='none',label='n(PC)='+str(nPC))
-    plt.legend(loc='right', fontsize=10)
-    plt.xlabel('rank')
-    plt.ylabel('relative variance', color='b')
-    plotstr = 'eigenspectrum'+plotstem
-    plt.savefig(plotstr)
-    plt.close('all')
-
-    fig,ax = plt.subplots()
-    for i in range(2*n):
-        labelstr_c = 'PC1 (constrained) ' + str(i+1)
-        plt.plot(dX['dX0_constrained'][i,:]/Xu, lw=2, label=labelstr_c)
-    for i in range(2*n):
-        labelstr_u = '(unconstrained) ' + str(i+1)
-        plt.plot(dX['dX0_unconstrained'][i,:]/Xu, '.', label=labelstr_u)
-#    plt.ylim(-2,2)
-    plt.legend(loc=2, fontsize=6, ncol=4)
-    plt.xlabel('parameter, a(n)')
-    plt.ylabel(r'$\delta a(n)/u(n)$')
-    plt.tight_layout()
-    plotstr = 'pc1_deltas_over_Xu'+plotstem
-    plt.savefig(plotstr)
-    plt.close('all')
-
-    fig,ax = plt.subplots()
-    for i in range(2*n):
-        labelstr_c = 'PC2 (constrained) ' + str(i+1)
-        plt.plot(dX['dX1_constrained'][i,:]/Xu, lw=2, label=labelstr_c)
-    for i in range(2*n):
-        labelstr_u = '(unconstrained) ' + str(i+1)
-        plt.plot(dX['dX1_unconstrained'][i,:]/Xu, '.', label=labelstr_u)
-#    plt.ylim(-2,2)
-    plt.legend(loc=2, fontsize=6, ncol=4)
-    plt.xlabel('parameter, a(n)')
-    plt.ylabel(r'$\delta a(n)/u(n)$')
-    plt.tight_layout()
-    plotstr = 'pc2_deltas_over_Xu'+plotstem
-    plt.savefig(plotstr)
-    plt.close('all')
-
-    if FLAG_crs_test:
-
-        for n in np.array([10,50,100,500,1000,5000,10000,50000]):
-            
-            random_numbers_unconstrained = crs.generate_10_single(n)
-            random_numbers_constrained = crs.generate_10(n)
-
-            fig,ax = plt.subplots(1,2)
-            labelstr_constrained = 'n=' + str(n) + ': constrained'
-            labelstr_unconstrained = 'n=' + str(n) + ': unconstrained'
-            ax[0].plot(np.sort(np.array(random_numbers_unconstrained)), label=labelstr_unconstrained)
-            ax[0].plot(np.sort(np.array(random_numbers_constrained)), label=labelstr_constrained)
-            ax[0].legend(loc=2, fontsize=8)
-            ax[0].set_ylim(-5,5)
-            ax[0].set_ylabel('z-score')
-            ax[0].set_xlabel('rank')
-            ax[0].set_title(r'Sorted random sample from $erf^{-1}(x)$')
-            ax[1].hist(random_numbers_unconstrained,bins=100,alpha=0.3,label='unconstrained')
-            ax[1].hist(random_numbers_constrained,bins=100,alpha=0.3,label='constrained')
-            ax[1].set_xlim(-5,5)
-            ax[1].set_xlabel('z-score')
-            ax[1].set_ylabel('count')
-            plotstr = 'random_numbers_n_' + str(n) + '.png'
-            plt.tight_layout()
-            plt.savefig(plotstr)
-            plt.close('all')
-
+    #------------------------------------------------        
     print('** end')
+
+
 
 
 
