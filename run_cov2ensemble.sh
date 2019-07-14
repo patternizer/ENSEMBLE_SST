b98924e8835a209495223a34a5f373c0ff23428c
Michael Taylor <patternizer@gmail.com>
2019-07-15 00:52:47 +0100

    Bsub for running cov2ensemble.py

---
+++ run_cov2ensemble.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# PROGRAM: run_cov2ensemble.sh
+#
+# Copyright (C) 2019 Michael Taylor, University of Reading                                 
+# This code was developed for the EC project "Fidelity and Uncertainty in                 
+# Climate Data Records from Earth Observations (FIDUCEO).                                 
+# Grant Agreement: 638822                                                                 
+
+# The code is distributed under terms and conditions of the MIT license:
+# https://opensource.org/licenses/MIT.
+
+echo '. /gws/nopw/j04/fiduceo/Users/mtaylor/anaconda3/bin/activate mike' > run.cov2ensemble.sh
+echo python cov2ensemble.py >> run.cov2ensemble.sh
+
+bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.1.log < run.cov2ensemble.sh
+
+
+
